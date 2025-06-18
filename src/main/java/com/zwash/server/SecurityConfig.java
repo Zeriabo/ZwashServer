e7c605ea020aae7e397e679d6732b98e20927b76
Zeriab Chiah <zeriab@Zeriabs-MBP.lan>
2025-06-18 23:31:43 +0300

    fixed packaging

--- src/main/java/com/zwash/server/SecurityConfig.java
+++ src/main/java/com/zwash/server/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.zwash;
+package com.zwash.server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/zwash/server/ZwashServerApplication.java
+++ src/main/java/com/zwash/server/ZwashServerApplication.java
@@ -1,4 +1,4 @@
-package com.zwash;
+package com.zwash.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
